6137288c73a3191efea41a4c5414abc454fd5dc4
Taras Osypchuk <osupa@ukr.net>
2016-09-09 13:01:36 +0300

    delete some results

--- Main.java
+++ Main.java
@@ -5,12 +5,7 @@ public class Main {
     public static void main (String[] args){
         TemperatureConverter TC = new TemperatureConverter();
         TC.convertTemp(30,'F');
-        TC.convertTemp(40,'F');
-        TC.convertTemp(50,'F');
-        TC.convertTemp(30,'C');
-        TC.convertTemp(40,'C');
-        TC.convertTemp(50,'C');
-        TC.convertTemp(60,'R');
+
 
     }
 
